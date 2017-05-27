b81c9846415d289e415bcf0919e432d0be303d16
Alex <alex.stancu.ct@gmail.com>
2017-05-27 23:50:22 +0300

    adaugat validare pentru toate campurile paginii de inregistrare

--- routes/users.js
+++ routes/users.js
@@ -23,11 +23,13 @@ router.post('/registration', function(req, res) {
   var email = req.body.email;
 
   // validarea formularului
+  req.checkBody('name', 'trebuie să îți introduci numele').notEmpty();
+  req.checkBody('username', 'trebuie să îți alegi un username cu cel puțin 4 caractere alfanumerice').len(4,20).isAlphaNum();
+  req.checkBody('email', 'o adresă valabilă de email este necesară').notEmpty().isEmail();
+  req.checkBody('password', 'parola trebuie să conțină cel puțin 6 caractere alfanumerice').len(6, 20).isAlphaNum();
+  req.checkBody('password', 'parola nu corespunde').equals(confirmPassword);
 
-req.checkBody('email', 'o adresă valabilă de email este necesară').notEmpty().isEmail();
-req.checkBody('password', 'parola trebuie să conțină cel puțin 6 caractere alfanumerice').len(6, 20).isAlphaNum();
-req.checkBody('password', 'parola nu corespunde').equals(confirmPassword);
-
+  // rezultatul validarii returnat ca promisiune
   req.getValidationResult().then(function(result){
     var errBoolean = result.isEmpty();
     var errors = result.mapped();
